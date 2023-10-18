8c2be171016b24beed49d2551d70886dc8fdfcae
Nikita <nikitatimoshin23@gmail.com>
2023-10-18 20:35:03 +0300

    feat: implemented user settings

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
+import { UserItem } from './user-item';
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -84,7 +85,7 @@ export const Navigation = () => {
       setIsResetting(true);
 
       sidebarRef.current.style.width = '0';
-      navbarRef.current.style.setProperty('width', '100%');
+      navbarRef.current.style.setProperty('width', 'w-[calc(100%-240px)]');
       navbarRef.current.style.setProperty('left', '0');
 
       setTimeout(() => setIsResetting(false), 300);
@@ -105,14 +106,14 @@ export const Navigation = () => {
           onClick={collapse}
           role="button"
           className={cn(
-            'h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-primary/10 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition',
+            'h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-primary/10 absolute top-[10px] right-2 opacity-0 group-hover/sidebar:opacity-100 transition',
             isMobile && 'opacity-100'
           )}
         >
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
@@ -120,13 +121,16 @@ export const Navigation = () => {
         <div
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
-          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+          className={cn(
+            'opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0',
+            isMobile && 'hidden'
+          )}
         />
       </aside>
       <div
         ref={navbarRef}
         className={cn(
-          'absolute top-0 z-[99999] left-60 w0[calc(100%-240px)]',
+          'absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]',
           isResetting && 'transition-all ease-in-out duration-300',
           isMobile && 'left-0 w-full'
         )}
